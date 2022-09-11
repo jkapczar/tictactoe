a63ce8cd7c49d4e93d6cd43da7de021f8d7b096f
jkapczar <janos.kapczar@icloud.com>
2022-09-11 17:20:38 +0200

    navigation

--- my-app/src/contaniers/home/Home.tsx
+++ my-app/src/contaniers/home/Home.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
+
     return(
-        <p>HOME</p>
+        <>
+            <p>HOME</p>
+            <button onClick={() => navigate("/game")}>GO to game</button>
+        </>
     )
 }
 
